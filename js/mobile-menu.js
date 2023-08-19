e2c91e7b111c85ab95f50eb413460dbce38344fd
Dari-phil <chekhov.d@gmail.com>
2023-08-20 00:04:35 +0300

    Add media

---
+++ js/mobile-menu.js
@@ -0,0 +1,18 @@
+const toggleMenu = () => {
+    const isMenuOpen =
+    openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    openMenuBtn.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+};
+openMenuBtn.addEventListener('click', toggleMenu);
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      body.classList.remove('no-scroll');
+  });
+})();
